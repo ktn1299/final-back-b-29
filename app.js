ab52f2abe6b30e17f76e4833dfe401c32fae36be
Tamilarasan <ktneditz1299@gmail.com>
2025-05-20 12:31:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,3 +31,8 @@ mongoose.connect(process.env.MONGODB_URI)
     });
   })
   .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
